db6838ed6b6376f08ba17c0b6e4f9f68c77b1020
Elton Kamgo Njomo <elton@panus.cm>
2022-11-25 18:19:15 +0100

    today's session

--- Panus.SharedClasses/LaboratoryOrder.cs
+++ Panus.SharedClasses/LaboratoryOrder.cs
@@ -9,11 +9,86 @@ namespace Panus.SharedClasses
     public class LaboratoryOrder : HospitalServiceOrder
     {
 
-        public LaboratoryOrder(DateTime dateOrdered, string specimenType)
+        public static string UnitName = "Laboratory";
+
+        public LaboratoryOrder(DateTime dateOrdered, string specimenType, string testType)
         {
             this.DateOrdered = dateOrdered;
             this.SpecimenType = specimenType;
+            this.TestType = testType;
+
         }
+
+        public LaboratoryOrder(DateTime dateOrdered)
+        {
+            this.DateOrdered = dateOrdered;
+        }
+
+       
         public string SpecimenType  { get; set; }
+
+        public string TestType { get; set; }
+
+
+        public void WhatUnitAreYouIn()
+        {
+            Console.WriteLine(UnitName);
+        }
+
+        public static void WhatIsTheDateOfToday()
+        {
+            Console.WriteLine(DateTime.Now);
+        } 
+
+        public static LaboratoryOrder CreateNew()
+        {
+            return new LaboratoryOrder(DateTime.Now, "Blood", "Malaria");
+        }
+
+        public static string WhichSpecimenIsRequired(string testType)
+        {
+            switch (testType)
+            {
+                case "Malaria":
+                    return "Blood";
+
+                case "Typhoid":
+                    return "Stool";
+
+                case "Ebola":
+                    return "Urine";
+
+                default:
+                    return "I don't know";
+
+            }
+        }
     }
+
+    public class Animal
+    {
+        public virtual string Speak()
+        {
+            return "Animal cannot speak";
+        }
+    }
+
+    public class Dog : Animal
+    {
+        public override string Speak()
+        {
+            return "I bark";
+        }
+    }
+
+    public class Cat : Animal
+    {
+        public override string Speak()
+        {
+            return "I meow";
+        }
+    }
+
+
+
 }
--- PassportApp/Program.cs
+++ PassportApp/Program.cs
@@ -12,61 +12,95 @@ namespace PassportApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
-            var ashu = new Person();
-            ashu.FirstName = "Ngaaje";
-            ashu.FamilyName = "Ngwese";
-            ashu.Height = 1.82;
+            //Console.WriteLine("Hello");
+            //var ashu = new Person();
+            //ashu.FirstName = "Ngaaje";
+            //ashu.FamilyName = "Ngwese";
+            //ashu.Height = 1.82;
 
-            var child1 = new Person();
-            child1.FirstName = "Ngaaje Jr";
-            child1.FamilyName = "Ngwese";
-            child1.DateOfBirth = new DateTime(1985,12,22);
-            Console.WriteLine(child1.HowOldWereYou(1990));
+            //var child1 = new Person();
+            //child1.FirstName = "Ngaaje Jr";
+            //child1.FamilyName = "Ngwese";
+            //child1.DateOfBirth = new DateTime(1985,12,22);
+            //Console.WriteLine(child1.HowOldWereYou(1990));
 
-            var child2 = new Person();
-            child2.FirstName = "Ashu Jr";
-            child2.FamilyName = "Ngwese";
+            //var child2 = new Person();
+            //child2.FirstName = "Ashu Jr";
+            //child2.FamilyName = "Ngwese";
 
-            Console.WriteLine(DateTime.Now);
-            Console.WriteLine(DateTime.Today);
-
-
-
-            //ashu.Children.AddRange(new[]{child1, child2, child2});
+            //Console.WriteLine(DateTime.Now);
+            //Console.WriteLine(DateTime.Today);
 
 
-            //var fxn = new Functions();
-            // Console.WriteLine(fxn.Linear(26.775M));
 
-            //var pharmacyOdrder = new PharmacyOrder();
-            //pharmacyOdrder.CreatedBy = "Paul";
-            //pharmacyOdrder.Unit = "Pharmacy";
+            ////ashu.Children.AddRange(new[]{child1, child2, child2});
 
 
-            //var laboratoryOrder = new LaboratoryOrder();
-            //laboratoryOrder.CreatedBy = "Peter";
-            //laboratoryOrder.Unit = "Laboratory";
-            //laboratoryOrder.SpecimenType = "Blood";
+            ////var fxn = new Functions();
+            //// Console.WriteLine(fxn.Linear(26.775M));
 
-            //var consultationOrder = new OutPatientDepartmentOrder();
-            //consultationOrder.Unit = "OutPatient Department";
-            //consultationOrder.DoctorConsulted = "Dr.Vera";
+            ////var pharmacyOdrder = new PharmacyOrder();
+            ////pharmacyOdrder.CreatedBy = "Paul";
+            ////pharmacyOdrder.Unit = "Pharmacy";
 
-            //List<HospitalServiceOrder> orders = new List<HospitalServiceOrder>();
-            //orders.Add(pharmacyOdrder);
-            //orders.Add(laboratoryOrder);
-            //orders.Add(consultationOrder);
 
-            //foreach (var order in orders)
-            //{
-            //    Console.WriteLine($"The unit is {order.DoctorConsulted}");
-            //}
+            ////var laboratoryOrder = new LaboratoryOrder();
+            ////laboratoryOrder.CreatedBy = "Peter";
+            ////laboratoryOrder.Unit = "Laboratory";
+            ////laboratoryOrder.SpecimenType = "Blood";
 
-            var laboratoryOrder = new LaboratoryOrder(DateTime.Today, "blood");
-            //var pharmaOrderWithParameter = new PharmacyOrder(DateTime.Today);
-            Console.WriteLine(laboratoryOrder.DateOrdered);
-            Console.WriteLine(laboratoryOrder.SpecimenType);
+            ////var consultationOrder = new OutPatientDepartmentOrder();
+            ////consultationOrder.Unit = "OutPatient Department";
+            ////consultationOrder.DoctorConsulted = "Dr.Vera";
+
+            ////List<HospitalServiceOrder> orders = new List<HospitalServiceOrder>();
+            ////orders.Add(pharmacyOdrder);
+            ////orders.Add(laboratoryOrder);
+            ////orders.Add(consultationOrder);
+
+            ////foreach (var order in orders)
+            ////{
+            ////    Console.WriteLine($"The unit is {order.DoctorConsulted}");
+            ////}
+
+            //var laboratoryOrder = new LaboratoryOrder(DateTime.Today, "blood");
+            ////var pharmaOrderWithParameter = new PharmacyOrder(DateTime.Today);
+            //Console.WriteLine(laboratoryOrder.DateOrdered);
+            //Console.WriteLine(laboratoryOrder.SpecimenType);
+            
+
+        
+
+            //var laboratoryOrder = new LaboratoryOrder(new DateTime(2022, 11, 20), "Blood","Malaria");
+            //Console.WriteLine(laboratoryOrder.DateOrdered);
+            //Console.WriteLine(laboratoryOrder.SpecimenType);
+            //Console.WriteLine(laboratoryOrder.TestType);
+            //Console.WriteLine(LaboratoryOrder.UnitName);
+
+            //var laboratoryOrder2 = new LaboratoryOrder(new DateTime(2021, 11, 20), "Urine", "Typhoid");
+            //Console.WriteLine(laboratoryOrder2.DateOrdered);
+            //Console.WriteLine(laboratoryOrder2.SpecimenType);
+            //Console.WriteLine(laboratoryOrder2.TestType);
+            //Console.WriteLine(LaboratoryOrder.UnitName);
+            //laboratoryOrder2.WhatUnitAreYouIn();
+
+            //LaboratoryOrder.WhatIsTheDateOfToday();
+
+
+            //var expiryDate = new DateTime(2022, 10, 30).AddDays(7);
+            //Console.WriteLine(expiryDate);
+
+            //var labOrder = LaboratoryOrder.CreateNew();
+            //Console.WriteLine(labOrder.SpecimenType);
+
+            var sicknessType = "HIV";
+            var specimenType = LaboratoryOrder.WhichSpecimenIsRequired(sicknessType);
+            Console.WriteLine(specimenType);
+
+
+            var cat = new Cat();
+            Console.Write(cat.Speak());
+
             Console.ReadKey();
         }
  
